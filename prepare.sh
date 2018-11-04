76ed7571a2da3a321b5bf6bcee98111292bb553a
KwonWooChan <32948824+KwonWooChan@users.noreply.github.com>
2018-11-05 04:33:06 +0900

    Create prepare.sh
    npm install for necessary modul.

---
+++ prepare.sh
@@ -0,0 +1,17 @@
+npm init -y
+
+npm install --save express
+
+npm install --save nedb
+
+npm install --save superagent
+
+
+npm i --save react react-dom
+npm i --save-div webpack
+npm i --save-dev babel-loder babel-core
+npm i --save-dev babel-preset-es2015 babel-preset-react
+
+npm install
+
+npm run build
